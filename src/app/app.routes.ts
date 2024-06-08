2b94551e008ecd6ba27d6339aba2baa378d78192
DEVLE <118941581+JaniceDevle@users.noreply.github.com>
2024-06-08 23:08:16 +0200

    Cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,4 +33,5 @@ export const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CartService } from '../services/cart.service';
+import { ProductModel } from '../models/productModel';
 
 @Component({
   selector: 'app-checkout',
@@ -16,14 +16,25 @@ export class CheckoutComponent implements OnInit {
   tax: number = 0;
   total: number = 0;
 
-  constructor(private router: Router, private cartService: CartService) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.calculateSummary();
+    this.#fetchProducts().then(x => {
+      x.forEach(i => {
+        (i as any).quantity = 1;
+      })
+      this.items = x;
+      this.calculateSummary();
+    });
+  }
+
+  async #fetchProducts() {
+    const res = await fetch('http://localhost:8000/api/products');
+    const data: ProductModel[] = await res.json();
+    return data as ProductModel[];
   }
 
   calculateSummary() {
-    this.items = this.cartService.getItems();
     this.subtotal = this.items.reduce((acc, item) => acc + (item.price * item.quantity), 0);
     this.tax = this.subtotal * 0.05;
     this.total = this.subtotal + this.tax;
@@ -34,7 +45,10 @@ export class CheckoutComponent implements OnInit {
   }
 
   remove(itemId: number) {
-    this.cartService.removeItem(itemId);
+    const index = this.items.findIndex(item => item.id === itemId);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
     this.calculateSummary();
   }
 }
